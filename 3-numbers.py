e43cc076011045d0066b647c691d92ba34f97d01
pnikoletopoulou <tetanikoletopoulou@gmail.com>
2025-01-10 20:49:34 +0200

    Update 3-numbers.py

--- 3-numbers.py
+++ 3-numbers.py
@@ -1,12 +1,3 @@
-'''
-Ask user for 3 numbers, a, b, and c. First number is the starting point, 
-second number is the end point, and third number is the step. 
-Then, print values from a to b, with a c step. If a is greater than b, then step down from a to b. Examples:
-a: 3, b: 12, c: 3
-Output: 3, 6, 9, 12
-a: 20, b: 5, c: 4
-Output: 20, 16, 12, 8
-'''
 
 print("Give three numbers!")
 
@@ -23,5 +14,3 @@ for i in range(a, b+1, c):
 if a > b:
     for i in range(a, b-1, -c):
         print(i)
-
-# DONE
