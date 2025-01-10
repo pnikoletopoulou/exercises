a0abdf333284a794c7835a5535ecb9f9f2448ebe
pnikoletopoulou <tetanikoletopoulou@gmail.com>
2025-01-10 20:50:36 +0200

    Update list-students.py

--- list-students.py
+++ list-students.py
@@ -1,13 +1,3 @@
-'''
-Given a list of students, generate usernames for each one of them and put
-them in another list. In case a username already exists, due to name similarity, use a counter to
-add a number at the end and increase the counter.
-Example:
-students = [‘John Doe’, ‘Mary Smith’, ‘Andrew Green’, ‘Lisa Tomas’,
-‘Mike Smith’, ‘Alex Green’]
-result will be => [‘jdoe’, ‘msmith’, ‘agreen’, ‘ltomas’, ‘msmith1’,
-‘agreen2’]
-'''
 
 students = ['John Green', 'Alex Tomas', 'Mary Green', 'Anna Smith', 
             'Mike Tomas', 'Andrew Green']
@@ -27,6 +17,3 @@ for student in students:
     fullnames.append(fullname)
 
 print(fullnames)
-   
-   
-     
